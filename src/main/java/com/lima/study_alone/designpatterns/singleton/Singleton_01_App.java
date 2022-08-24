c89218104b2f78e985a19250049a874d5d050a48
kimyoulim <kimyoulim1016@gmail.com>
2022-08-24 22:00:46 +0900

    백기서 선생님 - singletone pattern

---
+++ src/main/java/com/lima/study_alone/designpatterns/singleton/Singleton_01_App.java
@@ -0,0 +1,11 @@
+package com.lima.study_alone.designpatterns.singleton;
+
+public class Singleton_01_App {
+
+  public static void main(String[] args) {
+    Singleton_01 one = Singleton_01.getInstance();
+    Singleton_01 two = Singleton_01.getInstance();
+    System.out.println(one == two);
+
+  }
+}
