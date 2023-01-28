a579e198d6f879e23a980be758ff86f40d333b1a
kimyoulim <kimyoulim1016@gmail.com>
2023-01-28 23:44:00 +0900

    .

---
+++ src/main/java/com/lima/study_alone/codingtest/elevenstreet20230128/Test03.java
@@ -0,0 +1,11 @@
+package com.lima.study_alone.codingtest.elevenstreet20230128;
+
+public class Test03 {
+
+  public static void solution() {
+
+  }
+  public static void main(String[] args) {
+    Test03.solution();
+  }
+}
