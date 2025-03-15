b409f1ad3a226c879eeaac16d2b23c69165b5ad7
YoulimKim <kimyoulim1016@gmail.com>
2025-03-15 19:01:13 +0900

    .

--- src/main/java/com/lima/study_alone/codingtest/elevenstreet_20230128/test.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT DISTINCT c.group_name                                                            as name
-              , COUNT(c.group_name)                                                     as all_test_cases
-              , (length(c.status) - length(replace(c.status, 'OK', '')))                as passed_test_cases
-              , g.test_value * (length(c.status) - length(replace(c.status, 'OK', ''))) as total_value
-from test_cases c
-         inner join test_groups g on c.group_name = g.name
-group by c.status, c.group_name, g.test_value
